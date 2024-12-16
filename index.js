ed814dae38fc340ce9ed4a724bfe2539374ea09f
LorenzoLemmi <lorenzo.lemmi92@gmail.com>
2024-12-16 18:40:45 +0100

    feat: Add spinner and "loading" status on click

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 
 
 
+const submitButton = document.querySelector("form > button");
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault ()
+    submitButton.disable = true
+    submitButton.querySelector("#spinner").classList.toggle("d-none")
+    submitButton.querySelector(".status").innerHTML = `Loading...`
+})
 
 
